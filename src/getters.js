5fc56fee7ed8140a881ba08879b913da3498441b
Роман Ковжогин <r.kovjogin@gmail.com>
2020-04-24 15:34:41 +0300

    Добавил значение по-молчанию для state и getters

--- src/getters.js
+++ src/getters.js
@@ -1,8 +1,16 @@
-import {ALL_KEYS} from './const';
+import { ALL_KEYS, CURRENT_PAGE, DATA, FAIL_RESPONSE, FILTER, META, SUCCESS_RESPONSE, PER_PAGE, TOTAL_ITEMS,TOTAL_PAGES } from './const';
 import fromPairs from 'lodash-es/fromPairs';
-import toPairs from 'lodash-es/toPairs'
+import isObject from 'lodash-es/isObject';
+import isArray from 'lodash-es/isArray';
+
 
 export const mergeGetters = function (keys = ALL_KEYS, getters) {
+  const FILTER_KEY = keys[FILTER];
+  const META_KEY = keys[META];
+  const DATA_KEY = keys[DATA];
+  const SUCCESS_RESPONSE_KEY = keys[SUCCESS_RESPONSE];
+  const FAIL_RESPONSE_KEY = keys[FAIL_RESPONSE];
+
   return {
     ...fromPairs(Object.values(keys).map(key => [
       key,
@@ -10,6 +18,18 @@ export const mergeGetters = function (keys = ALL_KEYS, getters) {
     ])),
 
     keys: (state) => keys,
+
+    [DATA_KEY]: (state) => isArray(state[DATA_KEY]) ? state[DATA_KEY] : {},
+    [FILTER_KEY]: (state) => isObject(state[FILTER_KEY]) ? state[FILTER_KEY] : {},
+    [SUCCESS_RESPONSE_KEY]: (state) => isObject(state[SUCCESS_RESPONSE_KEY]) ? state[SUCCESS_RESPONSE_KEY] : {},
+    [FAIL_RESPONSE_KEY]: (state) => isObject(state[FAIL_RESPONSE_KEY]) ? state[FAIL_RESPONSE_KEY] : {},
+
+    [META_KEY]: (state) => isObject(state[META_KEY]) ? state[META_KEY] : {
+      [keys[CURRENT_PAGE]] : 1,
+      [keys[PER_PAGE]] : 10,
+      [keys[TOTAL_ITEMS]] : 0,
+      [keys[TOTAL_PAGES]] : 1,
+    },
 
     ...getters
   };
--- src/state.js
+++ src/state.js
@@ -3,9 +3,17 @@ import {
   IS_LOADING,
   CURRENT_PAGE,
   PER_PAGE,
+  META,
+  DATA,
+  FILTER,
+  TOTAL_ITEMS,
+  TOTAL_PAGES,
+  FAIL_RESPONSE,
+  SUCCESS_RESPONSE,
   IS_SUCCESS_RESPONSE,
   IS_FAIL_RESPONSE
 } from './const';
+
 import fromPairs from 'lodash-es/fromPairs';
 
 export const mergeState = function(keys = ALL_KEYS, state) {
@@ -13,11 +21,18 @@ export const mergeState = function(keys = ALL_KEYS, state) {
     keys: keys,
     ...fromPairs(Object.values(keys).map(key => [key])),
 
+    [keys[IS_SUCCESS_RESPONSE]]: false,
+    [keys[IS_FAIL_RESPONSE]]: false,
     [keys[IS_LOADING]]: false,
     [keys[CURRENT_PAGE]]: 1,
     [keys[PER_PAGE]]: 10,
-    [keys[IS_SUCCESS_RESPONSE]]: false,
-    [keys[IS_FAIL_RESPONSE]]: false,
+    [keys[TOTAL_ITEMS]]: 0,
+    [keys[TOTAL_PAGES]]: 1,
+    [keys[FILTER]]: {},
+    [keys[META]]: {},
+    [keys[DATA]]: [],
+    [keys[SUCCESS_RESPONSE]]: {},
+    [keys[FAIL_RESPONSE]]: {},
 
     ...state
   };
